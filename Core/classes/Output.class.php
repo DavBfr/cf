3d80cdee01f7a72d00085020301f1d4456934d30
David PHAM-VAN <dev.nfet.net@gmail.com>
2014-12-10 16:33:13 +0100

    Improve file() method

--- Core/classes/Output.class.php
+++ Core/classes/Output.class.php
@@ -53,10 +53,10 @@ class Output {
 	}
 
 
-	public static function file($filename, $data) {
+	public static function file($filename, $data, $type = "application/binary", $inline = false) {
 		header('Content-Description: File Transfer');
-		header('Content-Type: application/vnd.ms-excel');
-		header('Content-Disposition: attachment; filename='.$filename);
+		header('Content-Type: ' . $type);
+		header('Content-Disposition: '.($inline?'inline':'attachment').'; filename='.$filename);
 		header('Content-Transfer-Encoding: binary');
 		header('Expires: 0');
 		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
