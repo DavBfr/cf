719831e8e8920130be8c863c324741e8bad09e55
David PHAM-VAN <dev.nfet.net@gmail.com>
2014-12-18 19:37:09 +0100

    Revert angularjs to 1.2.28 and angular-strap to 2.1.4

--- Angular/www/vendor/angular-strap/modules/popover.tpl.js
+++ Angular/www/vendor/angular-strap/modules/popover.tpl.js
@@ -1,15 +1,14 @@
 /**
  * angular-strap
- * @version v2.0.0-rc.4 - 2014-03-07
+ * @version v2.1.4 - 2014-11-26
  * @link http://mgcrea.github.io/angular-strap
  * @author Olivier Louvignes (olivier@mg-crea.com)
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
-angular.module('mgcrea.ngStrap.popover').run(['$templateCache', function($templateCache) {
-  'use strict';
+'use strict';
 
-  $templateCache.put('popover/popover.tpl.html',
-    "<div class=\"popover\"><div class=\"arrow\"></div><h3 class=\"popover-title\" ng-bind=\"title\" ng-show=\"title\"></h3><div class=\"popover-content\" ng-bind=\"content\"></div></div>"
-  );
+angular.module('mgcrea.ngStrap.popover').run(['$templateCache', function($templateCache) {
+
+  $templateCache.put('popover/popover.tpl.html', '<div class="popover"><div class="arrow"></div><h3 class="popover-title" ng-bind="title" ng-show="title"></h3><div class="popover-content" ng-bind="content"></div></div>');
 
 }]);
